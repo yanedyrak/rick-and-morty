9f685aa20992767a6dcc904f9b4d6e3502a7a2c2
johan <kkzemeow@gmail.com>
2024-05-17 21:28:25 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import styles from "./App.module.scss";
 import Header from "./widgets/Header/Header";
 import Footer from "./widgets/Footer/Footer";
-import LinkContainer from "./entities/Episode/LinkContainer";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/Main/MainPage";
 import CharacterPage from "./pages/Character/CharacterPage";
