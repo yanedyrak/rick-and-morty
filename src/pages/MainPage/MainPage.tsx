86839e9a0b17155114521161afaddd7fb1bb430d
johan <kkzemeow@gmail.com>
2024-05-19 11:07:53 +0300

    update readme file

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,10 @@
 import styles from "./MainPage.module.scss";
 import Logo from "../../shared/assets/logo/Logo";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCategory } from "../../shared/store/slices/UI/categorySlice";
 const MainPage = () => {
+  const dispatch = useDispatch();
   return (
     <div className={styles.container}>
       <div className={styles.image}>
@@ -12,21 +15,33 @@ const MainPage = () => {
         Something information about Rick and Morty
       </p>
       <div className={styles.cards}>
-        <Link className={styles.link} to="/characters">
+        <Link
+          onClick={() => dispatch(setCategory(0))}
+          className={styles.link}
+          to="/characters"
+        >
           <h1 className={styles.cardTitle}>Characters</h1>
           <p className={styles.cardDescription}>
             Find information about character that you like
           </p>
         </Link>
 
-        <Link className={styles.link} to="/episodes">
+        <Link
+          onClick={() => dispatch(setCategory(1))}
+          className={styles.link}
+          to="/episodes"
+        >
           <h1 className={styles.cardTitle}>Episodes</h1>
           <p className={styles.cardDescription}>
             Find information about some episode
           </p>
         </Link>
 
-        <Link className={styles.link} to="/locations">
+        <Link
+          onClick={() => dispatch(setCategory(2))}
+          className={styles.link}
+          to="/locations"
+        >
           <h1 className={styles.cardTitle}>Locations</h1>
           <p className={styles.cardDescription}>
             Find the location of Rick and Morty
